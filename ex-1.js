a83218e154ae401a093560b8681145ba55dccacc
Tanawut Wongboot <tanawutwongboot@Mac-mini-khxng-Tanawut.local>
2023-12-13 10:19:33 +0700

    feat(ex-1): spread operator to merge object value into new variable

--- ex-1.js
+++ ex-1.js
@@ -12,5 +12,6 @@ let additionalProperties = {
 };
 
 // Start coding here
-
+let newStudentProfile = {};
+newStudentProfile = { ...studentProfile, ...additionalProperties };
 console.log(newStudentProfile);
