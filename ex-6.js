1a047463361c905a2bd812b615f5e6ea93b027c6
Tanawut Wongboot <tanawutwongboot@Mac-mini-khxng-Tanawut.local>
2023-12-13 10:48:06 +0700

    feat(ex-6): nullish coalescing

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,6 @@
 let height = undefined;
 let result;
 
+const func = (height) => height ?? "Height is not defined";
+result = func(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
